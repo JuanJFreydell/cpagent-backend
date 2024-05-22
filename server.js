be0d5f5ef69ed30735ebe6586c563c59e6872b27
Juan Freydell-Velez <juanfreydell-velez@Juans-MacBook-Air.local>
2024-05-21 22:58:02 -0700

    backend session2

--- server.js
+++ server.js
@@ -1,50 +1,55 @@
-const cors = require('cors');
-const express = require('express');
+const cors = require("cors");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const OpenAI = require("openai");
+const {
+  create_thread,
+  create_message,
+  run_thread,
+  list_messages,
+} = require("./utils/openai");
+const {
+  add_item,
+  get_item,
+  item_exists,
+} = require("./dao/in-memory-datastore");
+
 const app = express();
 const port = 8080;
-const { get_openai_client,
-        create_thread,
-        create_message,
-        run_thread,
-        list_messages } = require('./utils/openai')
-const { add_item, get_item, item_exists} = require('./dao/in-memory-datastore')
-
 app.use(cors());
-app.get('/register/:user', async (req, res) => {
-    console.log(req);
-    if (!item_exists("user:" + req.params.user)) {
-        add_item("user:" + req.params.user, "");
-    } 
-    res.json({"message": "welcome"});
-    console.log(res);
-})
-app.get('/:user/:message', async (req, res) => {
-    console.log(req);
-    if (!item_exists("user:" + req.params.user)) {
-        res.json({"message": "not registered yet."})
-        return;
-    } 
-    const openai = await get_openai_client();
 
-    var thread_id;
-    if (item_exists(req.params.user)) {
-        thread_id = get_item(req.params.user);
-    } else {
-        thread_id = await create_thread(openai);
-        add_item(req.params.user, thread_id);
-    }
-        await create_message(openai, thread_id, req.params.message)
-        // run the thread
-        await run_thread(openai, thread_id)
-        // wait till the thread is done running and list the messages
-        const data = await list_messages(openai, thread_id)
+const openai = new OpenAI({
+  apiKey: process.env.apiKey,
+});
 
-    
-    res.json({"message": data});
-    console.log(res);
-})
+app.get("/register/:user", async (req, res) => {
+  if (!item_exists("user:" + req.params.user)) {
+    add_item("user:" + req.params.user, "");
+  }
+  res.json({ message: "welcome" });
+});
+
+app.get("/:user/:message", async (req, res) => {
+  // if (!item_exists("user:" + req.params.user)) {
+  //     res.json({"message": "not registered yet."})
+  //     return;
+  // }
+  var thread_id;
+  if (item_exists(req.params.user)) {
+    thread_id = get_item(req.params.user);
+  } else {
+    thread_id = await create_thread(openai);
+    add_item(req.params.user, thread_id);
+  }
+  await create_message(openai, thread_id, req.params.message);
+  // run the thread
+  await run_thread(openai, thread_id);
+  // wait till the thread is done running and list the messages
+  const data = await list_messages(openai, thread_id);
+
+  res.json({ message: data });
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
-
+  console.log(`Example app listening on port ${port}`);
+});
--- utils/openai.js
+++ utils/openai.js
@@ -1,50 +1,34 @@
-const ASSISTANT_ID = "asst_hS2gwGqcdzjWEO8ONpNR3TtN"
-const OpenAI = require('openai')
-
-async function get_openai_client() {
-    const openai = await new OpenAI({
-        apiKey: ''
-    })
-
-    return openai;
-}
+const dotenv = require("dotenv").config();
 
 async function create_thread(openai) {
-    const thread = await openai.beta.threads.create();
-    return thread.id;
+  const thread = await openai.beta.threads.create();
+  return thread.id;
 }
 
 async function create_message(openai, thread_id, message) {
-    const threadMessages = await openai.beta.threads.messages.create(
-        thread_id,
-        {role: "user",
-        content: message}
-    );
-    return threadMessages
+  const threadMessages = await openai.beta.threads.messages.create(thread_id, {
+    role: "user",
+    content: message,
+  });
+  return threadMessages;
 }
 
 async function run_thread(openai, thread_id) {
-    const run = await openai.beta.threads.runs.create(
-        thread_id,
-        { assistant_id:  ASSISTANT_ID}
-    );
+  const run = await openai.beta.threads.runs.create(thread_id, {
+    assistant_id: process.env.assistantId,
+  });
 
-    var run_status;
-    do{
-        run_status = await openai.beta.threads.runs.retrieve(
-            thread_id,
-            run.id
-        );
-    }while (run_status.status!="completed")
+  var run_status;
+  do {
+    run_status = await openai.beta.threads.runs.retrieve(thread_id, run.id);
+  } while (run_status.status != "completed");
 
-    return; 
+  return;
 }
 
 async function list_messages(openai, thread_id) {
-    const threadMessages = await openai.beta.threads.messages.list(
-        thread_id
-    );
-    return threadMessages.body.data;
+  const threadMessages = await openai.beta.threads.messages.list(thread_id);
+  return threadMessages.body.data;
 }
 
-module.exports = {get_openai_client, create_thread, create_message, run_thread, list_messages }
+module.exports = { create_thread, create_message, run_thread, list_messages };
